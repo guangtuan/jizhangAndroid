a1ac509c39f8f64e739adb188357ad7ab66bc1ae
zhuangguangtuan <zhuangguangtuan@gmail.com>
2021-09-09 20:40:39 +0800

    feat: init base data after login

--- app/src/main/java/tech/igrant/jizhang/login/LoginActivity.kt
+++ app/src/main/java/tech/igrant/jizhang/login/LoginActivity.kt
@@ -14,6 +14,7 @@ import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import tech.igrant.jizhang.R
 import tech.igrant.jizhang.framework.RetrofitFacade
+import tech.igrant.jizhang.main.InitService
 import tech.igrant.jizhang.main.MainActivity
 import tech.igrant.jizhang.state.EnvManager
 
@@ -62,9 +63,11 @@ class LoginActivity : AppCompatActivity() {
                             )
                         )
                         EnvManager.init(EnvManager.State.ONLINE)
-                        runOnUiThread {
-                            MainActivity.start(this)
-                            finish()
+                        InitService.init().subscribe {
+                            runOnUiThread {
+                                MainActivity.start(this)
+                                finish()
+                            }
                         }
                     },
                     {
